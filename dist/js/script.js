60860c004cb9fb8bd9da7d6e542e37ca12e696bd
maopaww <109499650+maopaww@users.noreply.github.com>
2023-04-30 18:17:56 +0800

    tugas pbo

---
+++ dist/js/script.js
@@ -0,0 +1,27 @@
+/*
+
+TemplateMo 559 Zay Shop
+
+https://templatemo.com/tm-559-zay-shop
+
+*/
+
+'use strict';
+$(document).ready(function() {
+
+    // Accordion
+    var all_panels = $('.templatemo-accordion > li > ul').hide();
+
+    $('.templatemo-accordion > li > a').click(function() {
+        console.log('Hello world!');
+        var target =  $(this).next();
+        if(!target.hasClass('active')){
+            all_panels.removeClass('active').slideUp();
+            target.addClass('active').slideDown();
+        }
+      return false;
+    });
+    // End accordion
+
+
+});
